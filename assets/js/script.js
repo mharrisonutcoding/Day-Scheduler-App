b4385746e974510bd7915f1272df83bb4e30cc07
Mike Harrison <mharrisonutcoding@gmail.com>
2023-01-28 22:40:06 -0600

    updated logic for current hour, and iterations through time blocks, working on the logic for the changing of the classes based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
+// display current date and time
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+
+//save button and input
+$('.saveBtn').on("click", function() {
+    let value = $(this).siblings('textarea').val();
+    let key = $(this).parent().attr('id');
+  
+    localStorage.setItem(key, value);
+})
+
+$('.time-block').each(function(){
+    var id = $(this).attr('id');
+    
+    $('#'+ id + ' .description').val(localStorage.getItem(id));
+});
+
+
+  //current hour
+  function hourlyUpdate() {
+    var currentHour = moment().format('HH');
+
+    //loop for time block, updates css class based on current time
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id"));
+
+      
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  hourlyUpdate();
